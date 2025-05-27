3507890d178eb237bb6b4bded7713e938060fc01
inferno-sudo <infernoashwin@gmail.com>
2025-05-27 11:04:18 +1000

    a
    a

--- main.py
+++ main.py
@@ -3,8 +3,5 @@ import pandas as pd
 # Load the dataset
 df = pd.read_csv("athlete_events.csv")
 
-# Show the first 5 rows
-print(df.head())
-
-# Show the column names
-print(df.columns)
+print(df['Sport'].value_counts().head())
+print(df['Sex'].value_counts())
